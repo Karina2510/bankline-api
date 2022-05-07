66c9c8b598f9ace8f15f29c33551974c4df8d6b6
Karina Ferreira <96082465+Karina2510@users.noreply.github.com>
2022-05-06 22:31:38 -0300

    atualizacoes

--- src/main/java/com/dio/santander/bankline/api/dto/NovaMovimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/dto/NovaMovimentacao.java
@@ -10,24 +10,31 @@ public class NovaMovimentacao {
 	public String getDescricao() {
 		return descricao;
 	}
+	
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	
 	public Double getValor() {
 		return valor;
 	}
+	
 	public void setValor(Double valor) {
 		this.valor = valor;
 	}
+	
 	public MovimentacaoTipo getTipo() {
 		return tipo;
 	}
+	
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
 	}
+	
 	public Integer getIdConta() {
 		return idConta;
 	}
+	
 	public void setIdConta(Integer idConta) {
 		this.idConta = idConta;
 	}
